6365aa2514960a1d31d2808ac04f090e88776160
Whitney Lampkin <wlampkin.davis@hotmail.com>
2017-09-14 14:51:42 -0500

    Add project files.

---
+++ ConsumeMathService/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsumeMathService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
